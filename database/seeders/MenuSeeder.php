0e07b6f862e50088ad566ad7d332116715bdb7d7
fqueta <ger.maisaqui1@gmail.com>
2024-12-09 11:26:26 -0300

    09/12/2024 03

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -23,7 +23,7 @@ class MenuSeeder extends Seeder
                 'icon'=>'fa fa-tachometer-alt',
                 'actived'=>true,
                 'url'=>'painel',
-                'route'=>'home.admin',
+                'route'=>'home',
                 'pai'=>''
             ],
             [
