74721ed1acc241e68c8795b3ebb0b357e1484a49
fqueta <ger.maisaqui1@gmail.com>
2025-03-25 14:43:16 -0300

    25/03/2025

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 /**
- * 1|vohDWlfJFrs2l0mXJFX3SKxcBVcDZDhkkOqjfgtr3aab67ac
+ * 1|vohDWlfJFrs2l0mXJFX3SKxcBVcDZDhkkOqjfgtr3aab67aclad
  *
  */
 class AuthController extends Controller
